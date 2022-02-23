c333b2b6e85a19261e555a23bd8838cf7f628c20
Jacek <devjac@ogarnij.se>
2022-02-23 20:04:07 +0100

    add test mem

---
+++ app/models/ModelsAPI.java
@@ -0,0 +1,7 @@
+package models;
+
+import play.db.jpa.Model;
+
+public class ModelsAPI extends Model {
+    public Long x;
+}
--- app/service/ServiceApp.java
+++ app/service/ServiceApp.java
@@ -1,13 +1,15 @@
 package service;
 
+import java.util.ArrayList;
 import java.util.Date;
+import com.google.common.collect.Lists;
 import com.google.gson.Gson;
 
 import play.Logger;
 
 public class ServiceApp {
     public int add(int a, int b) {
-
+        ArrayList<Object> objects = Lists.newArrayList();
         String s = new Gson().toJson(new Date());
         Logger.info(s);
         Logger.info(s);
