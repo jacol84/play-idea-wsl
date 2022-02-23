4eb7ea6664527a4b7562e8946211139c8dd45bb6
Jacek <devjac@ogarnij.se>
2022-02-23 19:33:40 +0100

    add test

--- app/service/ServiceApp.java
+++ app/service/ServiceApp.java
@@ -1,7 +1,17 @@
 package service;
 
+import java.util.Date;
+import com.google.gson.Gson;
+
+import play.Logger;
+
 public class ServiceApp {
     public int add(int a, int b) {
+
+        String s = new Gson().toJson(new Date());
+        Logger.info(s);
+        Logger.info(s);
+        Logger.info(s);
         return a + b;
     }
 }
