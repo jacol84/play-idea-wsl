95ed2f532f57510c3a7ee38d4abc02240d59e9e7
Jacek <devjac@ogarnij.se>
2022-02-23 20:18:00 +0100

    run test on WSL

---
+++ test/service/ServiceAppTestA.java
@@ -0,0 +1,22 @@
+package service;
+
+import java.util.ArrayList;
+import org.junit.Test;
+import com.google.common.collect.Lists;
+
+import static org.junit.Assert.assertEquals;
+
+public class ServiceAppTestA {
+
+    @Test
+    public void add() {
+        //given
+        ServiceApp serviceApp = new ServiceApp();
+        ArrayList<Object> objects = Lists.newArrayList();
+        System.out.printf("objects"+objects);
+        //when
+        int add = serviceApp.add(2, 2);
+        //then
+        assertEquals(4, add);
+    }
+}
